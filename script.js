0a59dcb8c299f440c812d6daa3e61c1d64d7d457
Bj-Xie <86019494+Bj-Xie@users.noreply.github.com>
2021-07-14 00:37:56 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(".step2").hide();
 $(".step3").hide();
+$(".step4").hide();
 
 $(".choose-1").click(function() {
     $(".step1").hide();
@@ -9,7 +10,11 @@ $(".choose-1").click(function() {
 });
 
 $(".fix").click(function() {
-    
+    $(".step1").hide();
+    $(".step2").hide();
+    $(".step3").hide();
+    $(".step4").show();
+    $(".step4").css("background-color","red");
 });
 
 $(".start-button").click(function() {
@@ -18,7 +23,7 @@ $(".start-button").click(function() {
     $(".step2img").css("height", "400px");
 });
 
-$(".qwq").click(function() {
+$(".qwq").dblclick(function() {
     $(".treasure").show();
     $(".step1").hide();
     $(".step2").hide();
